dd093464ebd73412b3d5dc2e492f6674993d343d
Ergyon <benoitdoudard56@gmail.com>
2025-04-30 22:13:31 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+const arrowRight = document.getElementById('arrow-right')
+arrowRight.addEventListener('click', () => {
+	console.log('Fleche de droite')
+})
+
+
+const arrowLeft = document.getElementById('arrow-left')
+arrowLeft.addEventListener('click', () => {
+	console.log('Fleche de gauche')
+})
+
+let bannerImg = document.querySelector('banner-img')
